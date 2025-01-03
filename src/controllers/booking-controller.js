d469b9f26034366dbd2fb8c16a6bef8d48d8c4b0
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-01-03 14:10:06 +0530

    added the RabbitMQ config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,29 +1,45 @@
 const { BookingService } = require("../services/index");
 const { StatusCodes } = require("http-status-codes");
 
+const { createChannel, publishMessage } = require("../utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
+
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
-    console.log("from booking controller :", response);
-    return res.status(StatusCodes.OK).json({
-      message: "Successfylly completed the booking",
-      success: true,
-      error: {},
-      data: response,
-    });
-  } catch (error) {
-    console.log("from booking controller", error);
-    res.status(error.statusCodes).json({
-      message: error.message,
-      success: false,
-      error: error.explanation,
-      data: {},
+class BookingController {
+  constructor() {}
+
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    const data = {
+      message: "Success",
+    };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    return res.status(200).json({
+      message: "Successfully published the event",
     });
   }
-};
 
-module.exports = {
-  create,
-};
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      console.log("from booking controller :", response);
+      return res.status(StatusCodes.OK).json({
+        message: "Successfylly completed the booking",
+        success: true,
+        error: {},
+        data: response,
+      });
+    } catch (error) {
+      console.log("from booking controller", error);
+      res.status(error.statusCodes).json({
+        message: error.message,
+        success: false,
+        error: error.explanation,
+        data: {},
+      });
+    }
+  }
+}
+
+module.exports = BookingController;
